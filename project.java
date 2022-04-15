0c8ea403c658de0591ea8a1eeb354a6859980bd5
Isaac <Isaac@MSI>
2022-04-15 04:09:49 +0100

    Changed jtext field to jtext-area, converted back to arraylist,

--- project.java
+++ project.java
@@ -1,5 +1,8 @@
 package searchEngine;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,6 +14,10 @@ import java.util.Map;
 
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+
 
 /**
  *
@@ -31,33 +38,63 @@ public class SearchGUI extends javax.swing.JFrame{
     	JFileChooser filechoose = new JFileChooser();
         filechoose.setMultiSelectionEnabled(true);
         
+       // TextArea TextArea = new TextArea();
+		//System.out.print("Match Found in " +fileName);
+        JTextArea area=new JTextArea();  
+    
+
+        JScrollPane pane = new JScrollPane(area);
+        pane.setPreferredSize(new Dimension(500, 200));
+        pane.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        
+        
+        
         int section = filechoose.showOpenDialog(jLabel1);
         if(section == JFileChooser.APPROVE_OPTION){
             File[] multiFile = filechoose.getSelectedFiles();
             String fileName = " ";
             long count = multiFile.length;
-            System.out.print("Number of files is" + count);
+            //System.out.print("Number of files is" + count);
             for(File file: multiFile){
             	BufferedReader inputStream =null;
             	String liner;
             	String data = jTextField1.getText();
             	Map<String, String> fileIndexer = new HashMap<String,String>();
-            	for(int i = 0; i < count; i++) {
-            	try {
-            		inputStream = new BufferedReader(new FileReader(file));
+            		try {
+            		inputStream = new BufferedReader(new FileReader(multiFile[0]));
+            		//System.out.print(multiFile[1]);
+            		
+            		
             		while((liner=inputStream.readLine()) != null) {
+            			
+            			/*char[] arrline = new char[liner.length()];
+            			
+            			for(i =0; i< liner.length(); i++) {
+            				arrline[i] = liner.charAt(i);
+            				
+            			}//
+            			/*for(char c : arrline) {
+            				System.out.print(c);
+            			}*/
             			//String[] parts = liner.split(":");
             			//jLabel5.setText("File Contains" + liner);
             			//System.out.print(liner);
             			if(liner.contains(data)){
-            				fileIndexer.put(file.getName(), file.getAbsolutePath());
-            				System.out.print("Match Found in " +fileName);
-            				jTextPane1.setText("Match Found in \n" + multiFile[i] + "\n on line: " +liner);
+            				
+            				//fileIndexer.put(file.getName(), file.getAbsolutePath());
+            				//JTextArea jTextArea = new JTextArea();
+							//System.out.print("Match Found in " +fileName);
+            				
+            				//textArea1.setText("Match Found in \n" + multiFile[0] + "\n on line"+liner);
+            				textArea1.append("Match Found in \n" +multiFile[0] + textArea1.getText());
+            			 
             				//System.out.print(fileIndexer);
             			}
             			else {
-            				System.out.print("No Match found in" +fileName);
-            				jTextPane1.setText("No Match found in\n" +multiFile[i]);            				
+            				//System.out.print("No Match found in" +fileName);
+            				   
+            				textArea1.setText("No match found in \n" + multiFile[0]);  
             			}
             		}
             	}catch(IOException e) {
@@ -73,15 +110,55 @@ public class SearchGUI extends javax.swing.JFrame{
 						}
             		}
             	}
+             	try {
+            		inputStream = new BufferedReader(new FileReader(multiFile[1]));
+            		//System.out.print(multiFile[1]);
+            		
+            		
+            		while((liner=inputStream.readLine()) != null) {
+            			
+
+            			if(liner.contains(data)){
+            				fileIndexer.put(file.getName(), file.getAbsolutePath());
+            				//System.out.print("success");
+            				
+            				//textArea1.setText("Match Found in \n" + multiFile[1] + "\n on line"+liner);
+            				textArea1.append("Match Found in \n" +multiFile[1] + textArea1.getText());
+							//System.out.print("Match Found in " +fileName);
+            				//textArea1.setText("Match Found in \n" + multiFile[0] + "\n on line"+liner);   
+            				//System.out.print(fileIndexer);
+            			}
+            			else {
+            				
+							
+							textArea1.setText("No match found in \n "+ multiFile[1]);							//System.out.print("No Match found in" +fileName);
+            				//textArea.setText("No Match found in\n" +multiFile[0]);            				
+            			}
+            		}
+            	}catch(IOException e) {
+            		System.out.println(e);
+            	}
+            	finally {
+            		if(inputStream != null) {
+            			try {
+							inputStream.close();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+            		}
+            	}
+            	
                 fileName += file.getName() + "";
-            }
+                this.add(pane, BorderLayout.CENTER);
+            
             jLabel5.setText("The File(s) " + fileName +" Have been selected");
-        }}
+        }}//k
         else{
             jLabel5.setText("Canceled open");
  
             }
-	}
+}
 	
 
 
@@ -114,6 +191,7 @@ public class SearchGUI extends javax.swing.JFrame{
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
         jLabel5 = new javax.swing.JLabel();
+        textArea1 = new java.awt.TextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAlwaysOnTop(true);
@@ -157,63 +235,60 @@ public class SearchGUI extends javax.swing.JFrame{
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(148, 148, 148)
-                                .addComponent(jLabel1))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(138, 138, 138)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(148, 148, 148)
+                                        .addComponent(jLabel1))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(138, 138, 138)
+                                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addContainerGap()
+                                        .addComponent(jLabel2)))
+                                .addGap(7, 7, 7)
+                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE))
                             .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addGap(6, 6, 6)
-                                        .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                    .addComponent(jLabel2))))
-                        .addGap(7, 7, 7)
-                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel4)
-                                .addGap(0, 0, Short.MAX_VALUE))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 307, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jButton2)
-                                .addGap(8, 8, 8)))))
-                .addContainerGap(27, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(12, 12, 12)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
-                    .addComponent(jButton1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel4)
-                .addGap(27, 27, 27)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addContainerGap())
-        );
+                                        .addComponent(jLabel4)
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 307, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(jButton2)
+                                        .addGap(8, 8, 8)))))
+                        .addGroup(layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(textArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addContainerGap(27, Short.MAX_VALUE))
+            );
+            layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(12, 12, 12)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(18, 18, 18)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel2)
+                        .addComponent(jButton1))
+                    .addGap(12, 12, 12)
+                    .addComponent(jLabel4)
+                    .addGap(27, 27, 27)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jButton2))
+                    .addGap(44, 44, 44)
+                    .addComponent(textArea1, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
+                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(jLabel3)
+                    .addContainerGap())
+            );
 
         pack();
     }// </editor-fold>                      
@@ -233,7 +308,7 @@ public class SearchGUI extends javax.swing.JFrame{
         // TODO add your handling code here:
     	String data = jTextField1.getText();
     	
-    	jTextPane1.setText("The Term '" + data +"' Have been selected and will now be searched");
+    	//jTextPane1.setText("The Term '" + data +"' Have been selected and will now be searched");
     	
     		
     	}                                     
@@ -279,5 +354,6 @@ public class SearchGUI extends javax.swing.JFrame{
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextPane jTextPane1;
+    private java.awt.TextArea textArea1;
     // End of variables declaration                   
 }
