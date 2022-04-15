31f459a220bd90cd9018a60e83672562fc26f7c6
Isaac Barry <79987239+Isaacx86@users.noreply.github.com>
2022-04-15 18:07:40 +0100

    Create control.java
    Control class for java search engine

---
+++ control.java
@@ -0,0 +1,36 @@
+package searchEngine;
+// Control class for search engine, initalizing the java swing look and feels to add the atributed of 
+
+public class control {
+	public static void main(String[] args) {
+		
+		try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(SearchGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(SearchGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(SearchGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(SearchGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>*/
+
+        /* Create and display the form */ 
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	searchgui.setVisible(true);
+            }
+		
+		SearchGUI searchgui = new SearchGUI(); // Constructor for init and searchGUI function.
+		
+		searchgui.initComponents();
+	
+        }
+	}
